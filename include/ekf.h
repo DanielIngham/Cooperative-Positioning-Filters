e76a81bfb832a629062e3779e5d5c7b637b46327
Daniel <49901842+DanielIngham@users.noreply.github.com>
2025-05-02 16:59:26 +0200

    Add documentation to the fields in EKF::EstimationParameters. The formulas and assumptions for the estimation parameters are documented.

--- include/ekf.h
+++ include/ekf.h
@@ -45,9 +45,10 @@ private:
   struct EstimationParameters {
 
     /**
-     * @brief Estimated robot state: x-coordinate, y-coordinate, orientation.
-     * @details \f[begin{bmatrix} x & y & \theta
-     * \end{bmatrix}^\top\f]
+     * @brief Estimated robot state.
+     * @details The state vector of the robot take the form \f[\begin{bmatrix} x
+     * & y & \theta \end{bmatrix}^\top, \f] where \f$x\f$ and \f$y\f$ denotes
+     * the robots 2D coordinates; and \f$\theta \f$ denotes the robots heading.
      */
     Eigen::Matrix<double, total_states, 1> state_estimate =
         Eigen::Matrix<double, total_states, 1>::Zero();
@@ -58,8 +59,14 @@ private:
      * therefore the prior estimation error covariance is initialised to a small
      * value.
      */
-    Eigen::Matrix<double, total_states, total_states> error_covarince =
+    Eigen::Matrix<double, total_states, total_states> error_covariance =
         Eigen::Matrix<double, total_states, total_states>::Identity() * 0.001;
+
+    /**
+     * @brief Measurement correction innovation matrix.
+     */
+    Eigen::Matrix<double, total_states, total_states> innovation =
+        Eigen::Matrix<double, total_states, total_states>::Zero();
 
     /**
      * @brief Kalman gain.
@@ -68,45 +75,79 @@ private:
         Eigen::Matrix<double, total_states, total_states>::Zero();
 
     /**
-     * @brief Odometry process noise: forward velocity, angular velocity.
-     * @details \f[v & \omega \f]
+     * @brief Odometry process noise covariance matrix.
+     * @details The process noise covariance matrix is defined by the
+     * expression:
+     * \f[ w = \begin{bmatrix} q_v & 0 \\ 0 & q_\omega \end{bmatrix}, \f] where
+     * \f$q_v\f$ denotes the forward velocity noise variance; and
+     * \f$q_\omega\f$ denotes the angular velocity noise variance.
+     * @note The process noise is assumed to be uncorrelated and therefore the
+     * covariance between the forward velocity and the angular velocity is
+     * assumed to be zero.
      */
     Eigen::Matrix<double, total_inputs, 1> process_noise =
         Eigen::Matrix<double, total_inputs, 1>::Zero();
 
     /**
-     * @brief Measurement noise: range, bearing.
-     * @details \f[r & \phi \f]
+     * @brief Measurement noise covariance matrix.
+     * @details The matrix for the measurement noise covariance matrix take the
+     * form \f[\begin{bmatrix} q_r & 0 \\ 0 & q_\phi \end{bmatrix}, \f] where
+     * \f$q_r\f$ denotes the range noise variance; and \f$\phi_r\f$ denotes the
+     * bearing noise variance.
+     * @note The measurement noise is assumed to be uncorrelated and therefore
+     * the covariance between the range and bearing is assumed to be zero.
      */
     Eigen::Matrix<double, total_measurements, 1> measurement_noise =
         Eigen::Matrix<double, total_measurements, 1>::Zero();
 
     /**
      * @brief Jacobian matrix of the motion model.
+     * @details The formula used for the calculation of the motion model
+     * Jacobian takes the form:
+     * \f[ F = \begin{bmatrix} 1 & 0 & -\tilde{v}\Delta t \sin(\theta) \\ 0 & 1
+     * & \tilde{v} \Delta t \cos(\theta) \\ 0 & 0 & 1 \end{bmatrix}, \f]
+     * where \f$\theta\f$ denotes the heading (orientation) of the ego vehicle;
+     * and \f$\tilde{v}\f$ denotes the forward velocity. The forward velocity is
+     * a random variable with Gaussian distributed noise \f$\mathcal{N}(0,w)\f$
+     * , where \f$w\f$ is defined by the covariance matrix
+     * EKF::EstimationParameters.measurement_noise.
      */
     Eigen::Matrix<double, total_states, total_states> motion_jacobian =
         Eigen::Matrix<double, total_states, total_states>::Zero();
 
     /**
      * @brief Jacobian matrix of the process noise.
+     * @details The formula used for the calculation of the process noise
+     * Jacobian takes the form
+     * \f[L = \begin{bmatrix}\Delta t \cos(\theta) & 0 \\ \Delta t \sin(\theta)
+     * & 0 \\ 0 & \Delta t \end{bmatrix}, \f]
+     * where \f$\Delta t\f$ denotes the sample period; and \f$\theta\f$ denotes
+     * the heading (orientation) of the ego robot.
      */
     Eigen::Matrix<double, total_states, total_inputs> process_jacobian =
         Eigen::Matrix<double, total_states, total_inputs>::Zero();
     /**
      * @brief Jacobian of the measurement model.
+     * @details The formula used for the calculation of the Jacobian of the
+     * measurement matrix between ego vehicle \f$i\f$ and measured vehicle
+     * \f$j\f$ take the form
+     * \f[ H = \begin{bmatrix} \frac{-\Delta x}{d} & \frac{-\Delta y}{d} & 0 &
+     * \frac{\Delta x}{d} & \frac{\Delta y}{d} & 0 \\ \frac{\Delta y}{d^2} &
+     * \frac{-\Delta x}{d^2} & -1 & \frac{-\Delta y}{d^2} & \frac{\Delta x}{d^2}
+     * & 0 \end{bmatrix} \f] where \f$\Delta x = x_j - x_i\f$; \f$\Delta y = y_j
+     * - y_i\f$; and \f$\Delta d = \sqrt{\Delta x^2 + \Delta
+     * y^2}\f$.
      */
-    Eigen::Matrix<double, total_measurements, total_measurements>
-        measurment_jacobian = Eigen::Matrix<double, total_measurements,
-                                            total_measurements>::Zero();
+    Eigen::Matrix<double, total_measurements, 2 * total_states>
+        measurment_jacobian =
+            Eigen::Matrix<double, total_measurements, 2 * total_states>::Zero();
   };
 
-  void prediction(const Robot::Odometry &odometry,
-                  const Robot::State &prior_state,
-                  EstimationParameters &estimation_parameters);
+  void prediction(const Robot::Odometry &, const Robot::State &,
+                  EstimationParameters &);
 
-  void correction(const Robot::Odometry &odometry,
-                  const Robot::State prior_state,
-                  const EstimationParameters estimation_parameters);
+  void correction(const Robot::Measurement &, const Robot::State &,
+                  EstimationParameters &, const EstimationParameters &);
   /**
    * @brief Houses all estimation parameters for all robots.
    */
