32bcf3c098bd0293f8209678182730fc596da0d9
Daniel <49901842+DanielIngham@users.noreply.github.com>
2025-05-11 08:24:39 +0200

    Add skeleton for Gaussian Belief Propagation filtering.

--- include/gbp.h
+++ include/gbp.h
@@ -4,15 +4,26 @@
 #include "filter.h"
 
 #include <DataHandler/DataHandler.h>
+#include <vector>
 /**
  * @class GBP
  * @brief Gaussian Belief Propagation
  */
 class GBP : public Filter {
 public:
-  GBP(DataHandler &);
+  GBP(DataHandler &, const unsigned short int window_size = 2U);
   ~GBP();
 
+  void performInference();
+
 private:
+  unsigned short int window_size_;
+
+  std::vector<Filter::EstimationParameters> graph;
+
+  void prediction(const Robot::Odometry &,
+                  std::vector<Filter::EstimationParameters> &);
+  void correction(std::vector<EstimationParameters> &,
+                  const EstimationParameters &);
 };
 #endif // INCLUDE_INCLUDE_GBP_H_
