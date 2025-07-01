c32717bcb17ec09e8a2f70aca9b3a6c417a4f194
Daniel <49901842+DanielIngham@users.noreply.github.com>
2025-07-01 17:20:09 +0200

    Remove unused code.

--- src/information_filter.cpp
+++ src/information_filter.cpp
@@ -31,89 +31,6 @@ InformationFilter::~InformationFilter() {}
  * @param[in,out] ego_robot The parameters required by the
  * Information filter to perform the prediction step.
  */
-#if 0
-void InformationFilter::prediction(const Robot::Odometry &odometry,
-                                   EstimationParameters &ego_robot) {
-  const double sample_period = data_.getSamplePeriod();
-
-  /* Hold onto the previous estimate for use populating the information vector.
-   */
-  state_t prior_estimate = ego_robot.state_estimate;
-
-  /* Make the prediction using the motion model: 3x1 matrix. */
-  motionModel(odometry, ego_robot, sample_period);
-
-  /* Calculate the Motion Jacobian: 3x3 matrix. */
-  calculateMotionJacobian(odometry, ego_robot, sample_period);
-
-  /* Calculate the process noise Jacobian: 3x2 matrix. */
-  calculateProcessJacobian(ego_robot, sample_period);
-
-  state_t d =
-      ego_robot.state_estimate - ego_robot.motion_jacobian * prior_estimate;
-
-  /* Calculate the linearised process covariance and precision. */
-  covariance_t linearised_process_covariance =
-      ego_robot.process_jacobian * ego_robot.process_noise *
-      ego_robot.process_jacobian.transpose();
-
-  // precision_t linearised_process_precision =
-  //     linearised_process_covariance.ldlt().solve(precision_t::Identity());
-  precision_t linearised_process_precision =
-      computePseudoInverse(linearised_process_covariance);
-
-  /* Populate augmented precision. */
-  augmentedPrecision_t augmented_precision;
-
-  augmented_precision.topLeftCorner<total_states, total_states>() =
-      linearised_process_precision;
-
-  augmented_precision.topRightCorner<total_states, total_states>() =
-      -linearised_process_precision * ego_robot.motion_jacobian;
-
-  augmented_precision.bottomLeftCorner<total_states, total_states>() =
-      -ego_robot.motion_jacobian.transpose() * linearised_process_precision;
-
-  augmented_precision.bottomRightCorner<total_states, total_states>() =
-      ego_robot.motion_jacobian.transpose() * linearised_process_precision *
-      ego_robot.motion_jacobian;
-
-  /* Add the contribution of the of the prior precision. */
-  augmented_precision.bottomRightCorner<total_states, total_states>() +=
-      ego_robot.precision_matrix;
-
-  /* Populate information vector. */
-  augmentedInformation_t augmented_information;
-
-  augmented_information.head<total_states>() =
-      -linearised_process_precision * d;
-
-  augmented_information.tail<total_states>() =
-      ego_robot.motion_jacobian.transpose() * linearised_process_precision * d;
-
-  /* Add the contribution of the prior information. */
-  augmented_information.tail<total_states>() += ego_robot.information_vector;
-
-  ego_robot.precision_matrix = marginalise(augmented_precision);
-
-  ego_robot.error_covariance = computePseudoInverse(ego_robot.precision_matrix);
-
-  ego_robot.information_vector =
-      marginalise(augmented_information, augmented_precision);
-
-  static bool first = true;
-
-  if (!ego_robot.information_vector.allFinite() && first) {
-    first = false;
-    std::cout << "Critical Error" << std::endl;
-  }
-
-  ego_robot.state_estimate =
-      ego_robot.error_covariance * ego_robot.information_vector;
-}
-#endif
-
-#if 1
 void InformationFilter::prediction(const Robot::Odometry &odometry,
                                    EstimationParameters &ego_robot) {
 
@@ -139,7 +56,6 @@ void InformationFilter::prediction(const Robot::Odometry &odometry,
   ego_robot.information_vector =
       ego_robot.precision_matrix * ego_robot.state_estimate;
 }
-#endif // 0
 
 #ifdef DECOUPLED
 void InformationFilter::correction(EstimationParameters &ego_robot,
